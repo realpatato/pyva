26ab835276c87a284c3c92c3d444fc34032b983d
realpatato <jason.t.richardii@gmail.com>
2026-01-21 11:00:18 -0500

    Added ability to end the shell program

--- shell.py
+++ shell.py
@@ -2,6 +2,8 @@ import pyva
 
 while True:
     text = input("PYVA > ")
+    if text == "end":
+        break
     result, error = pyva.run("<Sys.in>", text)
     if error:
         print(error.as_string())
