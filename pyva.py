de7c096ff00598b1ab5dab4d4063a5f4221aa27f
realpatato <jason.t.richardii@gmail.com>
2026-01-21 12:37:37 -0500

    Began work on parser, created BinOp and Number nodes

--- pyva.py
+++ pyva.py
@@ -140,6 +140,54 @@ class Lexer:
             return Token(TT_INT, int(num_str))
         else:
             return Token(TT_FLOAT, float(num_str))
+        
+#########
+# NODES #
+#########
+
+class NumberNode:
+    def __init__(self, token):
+        self.token = token
+    
+    def __repr__(self):
+        return f"{self.token}"
+
+class BinaryOperationNode:
+    def __init__(self, left_node, operator_token, right_node):
+        self.left_node = left_node
+        self.operator_token = operator_token
+        self.right_node = right_node
+    
+    def __repr__(self):
+        return f"({self.left_node}, {self.operator_token}, {self.right_node})"
+
+##########
+# PARSER #
+##########
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.token_index = 1
+        self.advance()
+    
+    def advance(self):
+        self.token_index += 1
+        if self.token_index < len(self.tokens):
+            self.current_token = self.tokens[self.token_index]
+        return self.current_token
+
+    def factor(self):
+        token = self.current_token()
+        if token.type in (TT_INT, TT_FLOAT):
+            self.advance()
+            return NumberNode(token)
+
+    def term(self):
+        pass
+
+    def expression(self):
+        pass
 
 ####### 
 # RUN #
